c6c3e5f3836f8e37531ca956c2a9f81f2434127e
blurryroots <blurryroots@posteo.de>
2015-02-19 13:23:12 +0100

    removed boilerplate stuff

--- src/Game.lua
+++ src/Game.lua
@@ -2,8 +2,6 @@ require ("lib.lclass")
 
 require ("src.EventManager")
 
-require ("src.data.PositionData")
-
 require ("src.events.FocusGainedEvent")
 require ("src.events.FocusLostEvent")
 require ("src.events.KeyboardKeyDownEvent")
@@ -16,7 +14,6 @@ class "Game"
 
 -- Constructs a new game
 function Game:Game ()
-	--
 	self.eventManager = EventManager ()
 
 	self.eventManager:subscribe ("FocusGainedEvent", self)
@@ -26,65 +23,28 @@ function Game:Game ()
 	self.eventManager:subscribe ("MouseButtonDownEvent", self)
 	self.eventManager:subscribe ("MouseButtonUpEvent", self)
 	self.eventManager:subscribe ("ResizeEvent", self)
-
-	self.log = {}
 end
 
 -- Raises (queues) a new event
 function Game:raise (event)
-	--
 	self.eventManager:push (event)
 end
 
 -- Callback used by EventManager
 function Game:handle (event)
-	--
-	print ("trying to handle " .. type (event))
-	if event:getClass () == "MouseButtonUpEvent" then
-		table.insert (self.log, {
-			timestamp = tostring (os.date ("%c")),
-			ttl = 4,
-			tal = 0,
-			message = tostring (event)
-		})
-	end
 end
 
 -- Updates game logic
 function Game:onUpdate (dt)
-	--
 	self.eventManager:update (dt)
-
-	local toberemoved = {}
-	for i, v in pairs (self.log) do
-		if (v.ttl - v.tal) <= 0 then
-			table.insert (toberemoved, i)
-		else
-			v.tal = v.tal + dt
-		end
-	end
-	for i, v in pairs (toberemoved) do
-		table.remove (self.log, v)
-	end
 end
 
 -- Renders stuff onto the screen
 function Game:onRender ()
 	--
-	self:renderLog ()
 end
 
 -- Gets called when game exits. May be used to do some clean up.
 function Game:onExit ()
 	--
-end
-
-function Game:renderLog ()
-	--
-	local basepos = {x = 42, y = 42}
-	for i, v in pairs (self.log) do
-		local msg = "[" .. v.timestamp .. "]: " .. v.message
-		local y = basepos.y + (i - 1) * 21
-		love.graphics.print (msg, basepos.x, y)
-	end
 end
