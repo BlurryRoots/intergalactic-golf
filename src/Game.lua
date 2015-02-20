fb902291965f61e20a7dc673c97385fc1865e73a
blurryroots <blurryroots@posteo.de>
2015-02-20 05:30:54 +0100

    integration

--- src/Game.lua
+++ src/Game.lua
@@ -1,33 +1,75 @@
 require ("lib.lclass")
+require ("lib.yanecos.EntityManager")
+require ("lib.yagl.AssetManager")
 
 require ("src.EventManager")
 
+require ("src.events.BuildModeEndEvent")
+require ("src.events.BuildModeStartEvent")
 require ("src.events.FocusGainedEvent")
 require ("src.events.FocusLostEvent")
 require ("src.events.KeyboardKeyDownEvent")
 require ("src.events.KeyboardKeyUpEvent")
 require ("src.events.MouseButtonDownEvent")
 require ("src.events.MouseButtonUpEvent")
+require ("src.events.PlaySoundEvent")
 require ("src.events.ResizeEvent")
+
+require ("src.data.AnimationData")
+require ("src.data.GameData")
+require ("src.data.PlanetData")
+require ("src.data.TransformData")
+
+require ("src.processors.AnimationProcessor")
+require ("src.processors.BuildModeProcessor")
+require ("src.processors.MovementProcessor")
+require ("src.processors.PlayerInputProcessor")
+require ("src.processors.SoundProcessor")
+
+local inspect = require ("lib.inspect")
 
 class "Game"
 
 -- Constructs a new game
 function Game:Game ()
-	self.eventManager = EventManager ()
+	self.events = EventManager ()
+	self.events:subscribe ("FocusGainedEvent", self)
+	self.events:subscribe ("FocusLostEvent", self)
+	self.events:subscribe ("KeyboardKeyDownEvent", self)
+	self.events:subscribe ("KeyboardKeyUpEvent", self)
+	self.events:subscribe ("MouseButtonDownEvent", self)
+	self.events:subscribe ("MouseButtonUpEvent", self)
+	self.events:subscribe ("ResizeEvent", self)
 
-	self.eventManager:subscribe ("FocusGainedEvent", self)
-	self.eventManager:subscribe ("FocusLostEvent", self)
-	self.eventManager:subscribe ("KeyboardKeyDownEvent", self)
-	self.eventManager:subscribe ("KeyboardKeyUpEvent", self)
-	self.eventManager:subscribe ("MouseButtonDownEvent", self)
-	self.eventManager:subscribe ("MouseButtonUpEvent", self)
-	self.eventManager:subscribe ("ResizeEvent", self)
+	self.assets = AssetManager ()
+
+	self.assets:loadImage ("gfx/grass_tile.png", "gfx/tile/Grass")
+	self.assets:loadImage ("gfx/grass_tile.png", "gfx/tile/Bush")
+	self.assets:loadImage ("gfx/grass_tile.png", "gfx/tile/Plane")
+	self.assets:loadImage ("gfx/grass_tile.png", "gfx/tile/Barren")
+
+	self.assets:loadImage ("gfx/empty_tile.png", "gfx/tile/Empty")
+	self.assets:loadImage ("gfx/end_tile.png", "gfx/tile/End")
+	self.assets:loadImage ("gfx/grass_tile.png", "gfx/tile/Lawn")
+	self.assets:loadImage ("gfx/lake_tile.png", "gfx/tile/Lake")
+	self.assets:loadImage ("gfx/sand_tile.png", "gfx/tile/Sand")
+	self.assets:loadImage ("gfx/start_tile.png", "gfx/tile/Start")
+
+	self.entities = EntityManager ()
+	local gd =
+		self.entities
+			:addData (self.entities:createEntity ({"gamedata"}), GameData ())
+	gd.planets["Knurpsel"] = PlanetData ()
+
+	self.buildModeProcessor = BuildModeProcessor (self.entities, self.assets)
+	self.buildModeProcessor:startBuildMode (BuildModeStartEvent ("Knurpsel"))
+
+	self.animationProcessor = AnimationProcessor (self.entities, self.assets)
 end
 
 -- Raises (queues) a new event
 function Game:raise (event)
-	self.eventManager:push (event)
+	self.events:push (event)
 end
 
 -- Callback used by EventManager
@@ -36,12 +78,15 @@ end
 
 -- Updates game logic
 function Game:onUpdate (dt)
-	self.eventManager:update (dt)
+	self.events:update (dt)
+
+	self.buildModeProcessor:onUpdate (dt)
 end
 
 -- Renders stuff onto the screen
 function Game:onRender ()
-	--
+	self.buildModeProcessor:onRender ()
+	self.animationProcessor:onRender ()
 end
 
 -- Gets called when game exits. May be used to do some clean up.
