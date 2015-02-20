493959ac3ee4234daeb7991619ae2ffd6138436d
blurryroots <blurryroots@posteo.de>
2015-02-20 08:37:20 +0100

    proper price calculation

--- src/processors/BuildModeProcessor.lua
+++ src/processors/BuildModeProcessor.lua
@@ -12,28 +12,45 @@ function BuildModeProcessor:BuildModeProcessor (entities, events, assets)
 		x = PlanetData.TileSize / 2,
 		y = PlanetData.TileSize / 2
 	}
-
-	self.currentPlanet = nil
-	self.hoveredAnimation = nil
 end
 
 function BuildModeProcessor:onUpdate (dt)
-	if self.hoveredAnimation then
-		for y = 1, PlanetData.MapSize.Height do
-			for x = 1, PlanetData.MapSize.Width do
-				self.tileanimatios[y][x].color.g = 255
+	if not self.active then
+		return
+	end
+
+	for y = 1, PlanetData.MapSize.Height do
+		for x = 1, PlanetData.MapSize.Width do
+			self.tileanimatios[y][x].color.g = 255
+		end
+	end
+
+	if self.hoveredTilePosition then
+		self.tileanimatios[self.hoveredTilePosition.y][self.hoveredTilePosition.x]
+			.color.g = 0
+	end
+
+	self.total = 0
+	for y = 1, PlanetData.MapSize.Height do
+		for x = 1, PlanetData.MapSize.Width do
+			if self.currentPlanet.map[y][x] ~= self.buildmap[y][x] then
+				self.total = self.total + self.buildmap[y][x].price
 			end
 		end
-
-		self.hoveredAnimation.color.g = 0
 	end
 end
 
 function BuildModeProcessor:onRender ()
+	if not self.active then
+		return
+	end
+
+	love.graphics.print ("total: " .. self.total, self.mapOffset.x, 700)
 end
 
 function BuildModeProcessor:handle (event)
 	local name = event:getClass ()
+
 	if "StartBuildModeEvent" == name then
 		self:startBuildMode (event)
 		return
@@ -46,21 +63,44 @@ function BuildModeProcessor:handle (event)
 
 	if "MouseMovedEvent" == name then
 		self:checkHover (event)
+		return
+	end
+
+	if "MouseButtonDownEvent" == name then
+		self:onMousedown (event)
+		return
+	end
+
+	if "MouseButtonUpEvent" == name then
+		self:onMouseup (event)
+		return
 	end
 end
 
 function BuildModeProcessor:startBuildMode (event)
+	self.active = true
+
+	self.currentPlanet = nil
+	self.currentTileToBuild = nil
+	self.hoveredTilePosition = nil
+	self.buildmap = {}
+
 	local gd = self.entities:getData (
 		self.entities:firstEntityWithTag ({"gamedata"}),
 		GameData:getClass ()
 	)
 
-	-- add map tiles
-	self.currentPlanet = event.planetName
-	local planet = gd.planets[self.currentPlanet]
-	assert (planet, "no planet called " .. event.planetName)
+	self.currentPlanet = gd.planets[event.planetName]
+	assert (self.currentPlanet, "no planet called " .. event.planetName)
 
-	-- add tile menu
+	for y = 1, PlanetData.MapSize.Height do
+		self.buildmap[y] = {}
+		for x = 1, PlanetData.MapSize.Width do
+			self.buildmap[y][x] = self.currentPlanet.map[y][x]
+		end
+	end
+
+	-- add map tiles
 	local bg = self.entities:createEntity ({"build-menu"})
 
 	self.entities:addData (bg, TransformData (self.mapOffset.x, self.mapOffset.y))
@@ -72,7 +112,7 @@ function BuildModeProcessor:startBuildMode (event)
 	for y = 1, PlanetData.MapSize.Height do
 		self.tileanimatios[y] = {}
 		for x = 1, PlanetData.MapSize.Width do
-			local tile = planet.map[y][x]
+			local tile = self.currentPlanet.map[y][x]
 			local key = "gfx/tile/" .. tile.name
 			local ani = bganimation:addChild (AnimationData (key))
 			ani.offset.x = x - 1
@@ -82,20 +122,26 @@ function BuildModeProcessor:startBuildMode (event)
 		end
 	end
 
+	-- add tile menu
 	self.tilemenu = {}
 	for _, tile in pairs (PlanetData.TileType) do
 		local ani =
 			bganimation:addChild (AnimationData ("gfx/tile/"..tile.name))
 		ani.offset.x = PlanetData.MapSize.Width + 1
 		ani.offset.y = #self.tilemenu
+		ani.buttonname = tile.name
 
 		table.insert (self.tilemenu, ani)
 	end
 
 	self.events:subscribe ("MouseMovedEvent", self)
+	self.events:subscribe ("MouseButtonDownEvent", self)
+	self.events:subscribe ("MouseButtonUpEvent", self)
 end
 
 function BuildModeProcessor:endBuildMode (event)
+	self.active = false
+
 	local gd = self.entities:getData (
 		self.entities:firstEntityWithTag ({"gamedata"}),
 		GameData:getClass ()
@@ -106,9 +152,13 @@ function BuildModeProcessor:endBuildMode (event)
 	for _, eid in pairs (tiles) do
 		self.entities:deleteEntity (eid)
 	end
+	-- free shortcut lists
 	self.tileanimatios = nil
+	self.tilemenu = nil
 
 	self.events:unsubscribe ("MouseMovedEvent", self)
+	self.events:unsubscribe ("MouseButtonDownEvent", self)
+	self.events:unsubscribe ("MouseButtonUpEvent", self)
 end
 
 function BuildModeProcessor:checkHover (event)
@@ -120,9 +170,66 @@ function BuildModeProcessor:checkHover (event)
 	local hoversovermap = 0 < parts.x and parts.x <= PlanetData.MapSize.Width
 		and 0 < parts.y and parts.y <= PlanetData.MapSize.Height
 
-	self.hoveredAnimation = nil
+	self.hoveredTilePosition = nil
 	if hoversovermap then
-		--self.tileanimatios[parts.y][parts.x].color.g = 0
-		self.hoveredAnimation = self.tileanimatios[parts.y][parts.x]
+		self.hoveredTilePosition = {
+			x = parts.x,
+			y = parts.y
+		}
 	end
+end
+
+function BuildModeProcessor:isMenuButton (event)
+	local menupos = {
+		x = (PlanetData.TileSize * (PlanetData.MapSize.Width + 1)) + self.mapOffset.x,
+		y = PlanetData.TileSize
+	}
+	local ypart =
+		math.ceil ((event.position.y - self.mapOffset.y) / PlanetData.TileSize)
+
+	if
+		menupos.x < event.position.x
+		and event.position.x < (menupos.x + PlanetData.TileSize)
+		and 0 < ypart and ypart <= #self.tilemenu
+	then
+		return self.tilemenu[ypart].buttonname
+	end
+
+	return false
+end
+
+function BuildModeProcessor:isMapTile (event)
+	if not self.hoveredTilePosition then
+		return false
+	end
+
+	return self.hoveredTilePosition
+end
+
+function BuildModeProcessor:onMousedown (event)
+	-- check if menu is clicked
+	local tileButton = self:isMenuButton (event)
+	if tileButton then
+		print ("menu botton " .. tileButton .. " clicked")
+		self.currentTileToBuild = tileButton
+		return
+	end
+
+	-- check if map is clicked
+	local tilePos = self:isMapTile (event)
+	if tilePos then
+		if not self.currentTileToBuild then
+			print ("no tile selected!")
+			return
+		end
+		-- set tile type to selected type
+		self.buildmap[tilePos.y][tilePos.x] = PlanetData.TileType[self.currentTileToBuild]
+		-- set visual type
+		self.tileanimatios[tilePos.y][tilePos.x].key =
+			"gfx/tile/" .. self.buildmap[tilePos.y][tilePos.x].name
+		return
+	end
+end
+
+function BuildModeProcessor:onMouseup (event)
 end
